d5b64ca61a1567ae8a7ea0c5c6fe1457cbca4879
Adam Coddington <me@adamcoddington.net>
2017-01-08 02:37:45 -0800

    Updating to use ATMEGA2313; I needed just one more pin :-(.

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,25 +1,65 @@
 #include "main.h"
 
-#include <MAX31855.cpp>
+#include <SoftwareSerial.h>
+//#include <MAX31855.cpp>
 
-#define MAXDO   12
-#define MAXCS   4
-#define MAXCLK  13
-//Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
-//Adafruit_MAX31855 thermocouple(MAXCS);
-MAX31855 temp(MAXDO, MAXCS, MAXCLK);
+#define MAXDO   3
+#define MAXCLK  2
+#define RELAY   4
+
+
+SoftwareSerial Serial(1, 0);
+bool relayOn = false;
 
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
     //thermocouple.begin();
+    Serial.begin(9600);
+    pinMode(RELAY, OUTPUT);
+    pinMode(MAXDO, INPUT);
+    pinMode(MAXCLK, OUTPUT);
+}
+
+void enableRelay() {
+    digitalWrite(RELAY, HIGH);
+    relayOn = true;
+}
+
+void disableRelay() {
+    digitalWrite(RELAY, LOW);
+    relayOn = false;
+}
+
+void toggleRelay() {
+    if(relayOn) {
+        disableRelay();
+    } else {
+        enableRelay();
+    }
+}
+
+void getThermocoupleData() {
+    int bitCount = 32;
+    for(int i = 0; i < bitCount; i++) {
+        digitalWrite(MAXCLK, LOW);
+        delay(1);
+        Serial.print(digitalRead(MAXDO));
+        digitalWrite(MAXCLK, HIGH);
+        delay(1);
+    }
+    Serial.println("");
 }
 
 void loop() {
     //double temperature = thermocouple.readCelsius();
-    double temperature = temp.readJunction(CELSIUS);
-    double internal = temp.readThermocouple(CELSIUS);
+    //double temperature = temp.readJunction(CELSIUS);
+    //double internal = temp.readThermocouple(CELSIUS);
+    //delay(1000);
+    //Serial.println(temperature);
+    //Serial.println(internal);
+    Serial.println("READY");
+    getThermocoupleData();
     delay(1000);
-    Serial.println(temperature);
-    Serial.println(internal);
+    toggleRelay();
 }
--- software/src/main.h
+++ software/src/main.h
@@ -2,3 +2,6 @@
 
 void setup();
 void loop();
+void toggleRelay();
+void enableRelay();
+void disableRelay();
